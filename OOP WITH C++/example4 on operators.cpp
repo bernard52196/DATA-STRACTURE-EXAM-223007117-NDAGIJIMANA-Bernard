0006e3fb3f12de18b1afe8542c6d07e54c13d1d7
NDAGIJIMANA Bernard <ndgjmnbernard@gmail.com>
2025-02-21 09:00:29 +0200

    Add files via upload

---
+++ OOP WITH C++/example4 on operators.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    
+int a = 20, b = 2;
+cout << "Sum: " << a + b << endl;
+cout << "Difference: " << a - b << endl;
+cout << "Product: " << a * b << endl;
+cout << "Quotient: " << a / b << endl;
+cout << "Remainder: " << a % b << endl;
+
+return 0;
+
+}
+
